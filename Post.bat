0557713e7b27d76faae41d65603569043ddca5cb
unknown <novanuus@gmail.com>
2025-04-12 20:52:02 -0700

    Save progress

---
+++ Post.bat
@@ -0,0 +1,5 @@
+@echo off
+cd "F:\Games\MultiMC\instances\Enigmatica 2- Expert - E2E\.minecraft\saves\Tesmao"
+git add --all
+git commit -m "Save progress"
+git push
---
+++ Pre.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "F:\Games\MultiMC\instances\Enigmatica 2- Expert - E2E\.minecraft\saves\Tesmao"
+git pull
